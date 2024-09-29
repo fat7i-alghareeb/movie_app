65033c1dd6667fc3447c4af42850e805117490cb
fat7i-alghareeb <fat7ighareeb@gmail.com>
2024-09-29 08:38:06 +0300

    .

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,7 @@
 import 'dart:math';
 
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 class Constants {
-  static double borderRadius = 16.r;
+  static double borderRadius = 16;
   static const String apiKey = "e49da361";
   static const String dummyPoster =
       "https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2024/04/download-1.jpeg";
